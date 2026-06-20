b9dfb063f9a1900d92abf84af969912320c49084
Kathyayanidevops <madanakathyayani@gmail.com>
2026-06-20 18:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def confirmation(booking_id):
     return render_template('confirmation.html', booking=booking)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
